aeef8489119abb0d6d6b6f6931fc34ee7aa8b420
gkstnadl <gkstnadl12@naver.com>
2024-02-01 03:34:26 +0900

    모달창 뼈대 및 기능 간략히 구현

--- src/components/FanLetterList.jsx
+++ src/components/FanLetterList.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Detail from './Detail';
 
 function FanLetterList({ selectedMember, fanLetters }) {
-  if (!selectedMember) return null; // 선택된 멤버가 없는 경우(초기화면) 아무것도 표시하지 않음
+  const [selectedLetter, setSelectedLetter] = useState(null);
+  const [showDetail, setShowDetail] = useState(false);
+
+  const handleLetterClick = (letter) => {
+    setSelectedLetter(letter);
+    setShowDetail(true);
+  };
+
+  const handleCloseDetail = () => {
+    setShowDetail(false);
+    setSelectedLetter(null);
+  };
 
   return (
     <div>
       <h2>{selectedMember}님께 온 팬레터</h2>
       {fanLetters[selectedMember]?.map((letter) => (
-        <div key={letter.id}>
+        <div key={letter.id} onClick={() => handleCloseDetail(letter)}>
           <p>닉네임 : {letter.nickname}</p>
           <p>내용 : {letter.content}</p>
         </div>
       ))}
+      {showDetail && <Detail letter={selectedLetter} onClose={handleCloseDetail} />}
     </div>
   );
 }

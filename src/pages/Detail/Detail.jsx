b0dbc0eff6e824f6257cf09cf680a1c4014b753f
gkstnadl <gkstnadl12@naver.com>
2024-02-01 00:50:44 +0900

    모달 뼈대 조금 수정, 받아올 props 설정

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
-function Detail() {
+function Detail({ letter, onClose, selectedMember }) {
+  if (!letter) return null;
+
   return (
     <div className="modal">
-      <button>메인으로</button>
       <div className="modal-content">
-        <span className="close">&times;</span>
-        <h2>팬레터 상세보기</h2>
-        <p>
-          <strong>닉네임:</strong>
-        </p>
-        <p>
-          <strong>내용:</strong>
-        </p>
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <div>
+          <h2>{letter.nickname}</h2>
+          <p>작성 시간</p>
+        </div>
+        <p>To.{selectedMember}</p>
+        <div>{letter.content}</div>
       </div>
     </div>
   );
